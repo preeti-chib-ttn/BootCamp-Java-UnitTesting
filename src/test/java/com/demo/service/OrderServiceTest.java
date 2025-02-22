872af387d0b9f46066c1e40bfc1b577204805162
Preeti Chib <preeti.chib@tothenew.com>
2025-02-22 23:24:56 +0530

    final code

--- src/test/java/com/demo/service/OrderServiceTest.java
+++ src/test/java/com/demo/service/OrderServiceTest.java
@@ -1,17 +1,37 @@
 package com.demo.service;
+import com.demo.domain.Order;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OrderServiceTest {
-    @InjectMocks
-    OrderService orderService;
 
-    @InjectMocks
-    EmailService emailService;
+    private OrderService orderService;
+    private Order order;
+
+    @Before
+    public void setup() {
+        orderService = OrderService.getInstance();
+        order = new Order();
+        order.setPrice(100.0);
+    }
+
+    @Test
+    public void testPlaceOrder() {
+        orderService.placeOrder(order,"test@mail.com");
+        assertEquals(120.0, order.getPriceWithTax(), 0.01);
+        assertTrue(order.isCustomerNotified());
+    }
 
 
+    @Test(expected = RuntimeException.class)
+    public void testPlacedOrderWithoutCustomerID() {
+        orderService.placeOrder(order);
+    }
 
 }
 
